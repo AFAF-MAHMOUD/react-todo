14b67dc02a4d8d92bb02408624f685fc76df1dc3
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-03 09:56:39 +0100

    Updates

---
+++ precache-manifest.2abc323494034bc83a55a1a5dc669d29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73307ca8e3e86b90240cfc65c86129b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "e028a1b16122e7b0aa37",
+    "url": "/react-todo/static/css/main.6490d891.chunk.css"
+  },
+  {
+    "revision": "595e611618f782cc17db",
+    "url": "/react-todo/static/js/2.9f301548.chunk.js"
+  },
+  {
+    "revision": "e028a1b16122e7b0aa37",
+    "url": "/react-todo/static/js/main.9c772763.chunk.js"
+  },
+  {
+    "revision": "7c16f7d98ba1d2665d74",
+    "url": "/react-todo/static/js/runtime-main.822123c9.js"
+  }
+]);
